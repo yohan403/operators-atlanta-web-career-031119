0852a5ad618ee519a21f6bc2dd0e38271765c7f0
John Cho <johnycho16@gmail.com>
2019-02-25 03:05:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,15 +11,7 @@ end
 
 
 def not_safe?(speed)
-	speed < 40 || speed > 60 (? TRUE : FALSE)
+	speed < 40 || speed > 60 ? TRUE : FALSE
 end
 	
 
-
-unsafe?(30)
-unsafe?(50)
-unsafe?(70)
-
-not_safe?(30)
-not_safe?(50)
-not_safe?(70)
